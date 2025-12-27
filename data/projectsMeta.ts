1e97c2bda4cc6d75ba6a0373d2b3a39c278f30e0
PrincetonE123 <princeepeagba@gmail.com>
2025-12-27 13:07:35 -0600

    Updated the portfolio content and styling

--- data/projectsMeta.ts
+++ data/projectsMeta.ts
@@ -7,7 +7,7 @@ export const PINNED_REPOS = [
 
 // this categorizes repos by tags
 export const REPO_TAGS: Record<string, string[]> = {
-  "Fivos-AI-ChatApp": ["GUI", "WinForms App", "AI", "Frontend", "Backend", "Fullstack", "SQL", "Text-to-SQL", "JSON", "C#"],
+  "Fivos-AI-ChatApp": ["GUI", "WinForms App", "AI", "Frontend", "Backend", "Fullstack", "SQL", "Text-to-SQL", "JSON", "C#", "Python"],
   "Warehouse-Tracker": ["Frontend", "SQL", "Permission-Based Access", "Database Management", "WinForms App", "C#"],
   "AIconnect4": ["AI", "Game", "Uniform Cost Search", "AI Algorithms", "Javascript"],
   "Merge-Sort-Algorithm": ["Algorithm", "Sorting Algorithm", "C++", "Advanced Data Structures"],
@@ -18,7 +18,8 @@ export const REPO_TAGS: Record<string, string[]> = {
   "Dynamic-Programming": ["Algorithm", "Dynamic Programming", "C++", "Advanced Data Structures"],
   "Recursive-vs-Dynamic-Programming": ["Algorithm", "Dynamic Programming", "C++", "Advanced Data Structures"],
   "Recursive-vs.-Non-recursive": ["Algorithm", "Recursive", "Non-recursive", "C++", "Advanced Data Structures"],
-  "Brute-Force-Algorithm-vs.-Euclidian-Algorithm": ["Algorithm", "Brute Force", "Euclidean Algorithm", "C++", "Advanced Data Structures"]
+  "Brute-Force-Algorithm-vs.-Euclidian-Algorithm": ["Algorithm", "Brute Force", "Euclidean Algorithm", "C++", "Advanced Data Structures"],
+  "Princeton-Portfolio": ["Portfolio", "Web Development", "TypeScript", "JavaScript", "React", "Tailwind CSS", "Next.js", "Frontend"],
 };
 
 // these are all the tags available for filtering
